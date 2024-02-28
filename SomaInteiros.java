00443bec8d9b66543a58bc069327b92ff8cae21f
Ana Carolina <123709099+acmachado48@users.noreply.github.com>
2024-02-28 20:19:57 -0300

    Update SomaInteiros.java

--- SomaInteiros.java
+++ SomaInteiros.java
@@ -2,25 +2,20 @@ import java.util.Scanner;
 
 class SomaInteiros {
  public static void main (String args[]) {
-	Scanner entrada = new Scanner(System.in);
-	
-	//Declaracao de variaveis 
+	Scanner entrada = new Scanner(System.in); 
 	
 	int num1;
 	int num2;
 	int soma = 0;
 	
-	//Leituras
 	System.out.println("Digite um numero: ");
 	num1 = entrada.nextInt();
 	
 	System.out.println("Digite outro numero: ");
 	num2 = entrada.nextInt();
-	
-	//Somar 
+
 	soma = num1 + num2;
 	
-	//Mostrar na tela
 	System.out.println("Soma = "+soma);
 	
 	
